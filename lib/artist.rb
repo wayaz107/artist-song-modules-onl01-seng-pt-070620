cc0712ac356088aba15f497b2c0fac43b1b7aa93
Warda Ayaz <wayaz107@gmail.com>
2020-08-06 00:57:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
-    extend Findable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
