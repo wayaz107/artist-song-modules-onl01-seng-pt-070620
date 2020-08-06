743c63c8b900bfe82f94c455a7d8057a41aec1b5
Warda Ayaz <wayaz107@gmail.com>
2020-08-06 00:46:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods
+    extend Findable::ClassMethods
   attr_accessor :name
   attr_reader :artist
 
@@ -11,9 +12,9 @@ class Song
     @@all << self
   end
 
-  def self.find_by_name(name)
-    self.all.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   self.all.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@all
