90b3c67ebaea26073f166d3813ba89e4e8d0b23f
Warda Ayaz <wayaz107@gmail.com>
2020-08-06 00:44:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,18 +5,18 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+  @@all = []
 
   def initialize
-    @@songs << self
+    @@all << self
   end
 
   def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+    self.all.detect{|a| a.name == name}
   end
 
   def self.all
-    @@songs
+    @@all
   end
 
   def artist=(artist)
